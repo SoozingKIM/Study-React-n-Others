e2ae18b2f3cdd218b97b4fb5a45f07767988b1be
soojin <kimsj0012@naver.com>
2024-05-04 17:56:58 +0900

    프리티어 수정

--- 05-투자금계산기/src/App.jsx
+++ 05-투자금계산기/src/App.jsx
@@ -1,35 +1,37 @@
-import Header from "./components/header"
-import UserInput from "./components/UserInput"
+import Header from "./components/header";
+import UserInput from "./components/UserInput";
 import Results from "./components/Results";
 import { useState } from "react";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000 ,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
-  })
-  
-  const inputIsValid = userInput.duration>=1;
+  });
 
-  function handleChange(inputIdentifier, newValue){
-      setUserInput(prev=>{
-          return{
-              ...prev, 
-              [inputIdentifier]: +newValue
-          }
-      });
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prev) => {
+      return {
+        ...prev,
+        [inputIdentifier]: +newValue,
+      };
+    });
   }
 
   return (
     <>
-      <Header/>
-      <UserInput userInput={userInput} onChange={handleChange}/>
-      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p> }
-      {inputIsValid && <Results input={userInput}/>}
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
